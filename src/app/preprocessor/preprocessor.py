f84da574f99a22154d068d9d7e1ae37c8f6a2e8c
Simon Unterlugauer <simonunterlugauer@Laptop-von-Simon.local>
2024-06-01 10:36:13 +0200

    cleaning files

--- src/app/preprocessor/preprocessor.py
+++ src/app/preprocessor/preprocessor.py
@@ -48,14 +48,42 @@ class Preprocessor:
         return df_cleaned
     
 
-    def rm_second_loadgen_pod(self):
+    def rm_columns(self, string_to_be_removed):
         df = pd.read_csv(self.filepath)
 
-        columns_to_remove = [spalte for spalte in df.columns if 'loadgenerator.2' in spalte]
+        columns_to_remove = [spalte for spalte in df.columns if string_to_be_removed in spalte]
 
         df_cleaned = df.drop(columns_to_remove, axis=1)
 
         return df_cleaned
+    
+
+    def check_similarity(self, file_one, file_two):
+
+        df1 = pd.read_csv(file_one)
+        df2 = pd.read_csv(file_two)
+
+        # extract column names 
+        columns_df1 = df1.columns.tolist()
+        columns_df2 = df2.columns.tolist()
+
+        max_len = max(len(columns_df1), len(columns_df2))
+
+        print("Compare columns:")
+        print(f"{'CSV 1':<50} | {'CSV 2':<50}")
+        print("-" * 105)
+
+        for i in range(max_len):
+            spalte1 = columns_df1[i] if i < len(columns_df1) else ""
+            spalte2 = columns_df2[i] if i < len(columns_df2) else ""
+            if spalte1 != spalte2:  # show differences
+                print(f"{spalte1:<50} | {spalte2:<50}")
+
+        # check for extra columns
+        if len(columns_df2) > len(columns_df1):
+            print("\nAdditional Columns:")
+            for i in range(len(columns_df1), len(columns_df2)):
+                print(f"- {columns_df2[i]}")
 
 
 
