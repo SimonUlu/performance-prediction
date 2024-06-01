870ddc533c0140171c4d3961d888b7fd0c999858
Simon Unterlugauer <simonunterlugauer@Laptop-von-Simon.local>
2024-06-01 10:07:38 +0200

    created function to clean csv columns

--- src/app/preprocessor/preprocessor.py
+++ src/app/preprocessor/preprocessor.py
@@ -33,3 +33,29 @@ class Preprocessor:
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
             X, y, test_size=self.test_size, random_state=self.random_state)
+        
+
+    # function to clean csv files for unneccessary columns based on base case csv
+    def clean_csv(self, input_file, to_be_cleanded_file):
+        df_base = pd.read_csv(input_file)
+
+        columns = df_base.columns.tolist()
+
+        df_to_be_cleaned = pd.read_csv(to_be_cleanded_file)
+
+        df_cleaned = df_to_be_cleaned.reindex(columns = columns)
+
+        return df_cleaned
+    
+
+    def rm_second_loadgen_pod(self):
+        df = pd.read_csv(self.filepath)
+
+        columns_to_remove = [spalte for spalte in df.columns if 'loadgenerator.2' in spalte]
+
+        df_cleaned = df.drop(columns_to_remove, axis=1)
+
+        return df_cleaned
+
+
+
